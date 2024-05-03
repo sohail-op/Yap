4e332e28e99e1d07c568d1f7b8506c1f9be68418
sohail-op <67971304+sohail-op@users.noreply.github.com>
2024-05-03 11:30:41 +0530

    add errorHandler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 import connectDb from "./config/connectDb.js";
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is Live on http://localhost:${port}`);
