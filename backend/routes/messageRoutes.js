6546be90d3ff571c895f080c50cc92444d50487e
sohail-op <67971304+sohail-op@users.noreply.github.com>
2024-05-03 10:38:24 +0530

    add: validateToken to verify & protect routes.

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { getMessage, sendMessage } from "../controllers/messageController.js";
+import validateToken from "../middleware/validateTokenHandler.js";
 
 const router = express.Router();
 
-router.post("/send/:id", sendMessage);
-router.get("/:id", getMessage);
+router.post("/send/:id", validateToken, sendMessage);
+router.get("/:id", validateToken, getMessage);
 
 export default router;
