f75e4a721eb57453fdb611523b417b1b09ebfadc
sohail-op <67971304+sohail-op@users.noreply.github.com>
2024-05-03 11:29:23 +0530

    Update maxAge value to 7 days

--- backend/util/gentoken.js
+++ backend/util/gentoken.js
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken";
 
 export const gentoken = (userid, res) => {
   const token = jwt.sign({ userid }, process.env.JWT_SECRET, {
-    expiresIn: "15m",
+    expiresIn: "7d",
   });
 
-  res.cookie("jwt: ", token, {
-    maxAge: 15 * 60 * 1000, //ms
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, //in ms
     httpOnly: true,
     samesite: true,
   });
