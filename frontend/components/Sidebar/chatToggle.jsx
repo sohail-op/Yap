0470c98ea6fc8acf695a26995c99fd045195204c
sohail-op <67971304+sohail-op@users.noreply.github.com>
2024-10-09 23:56:10 +0530

    Update: add handleTabChange function to toggle between chat & setting

--- frontend/components/Sidebar/chatToggle.jsx
+++ frontend/components/Sidebar/chatToggle.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { twJoin } from "tailwind-merge";
+
+import Conversations from "./conversations";
+import Setting from "./setting";
 
 function ChatToggle() {
+  const [activeTab, setActiveTab] = useState("chat");
+  function handleTabChange(tab) {
+    setActiveTab(tab);
+  }
+
   return (
     <>
       <div className="flex rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md border-separate mt-3 w-full">
-        <div className="rounded-xl flex bg-blue-500 w-1/2 justify-center">
+        {/* chat div */}
+        <div
+          className={twJoin(
+            "rounded-xl flex w-1/2 justify-center",
+            activeTab === "chat" ? "bg-blue-500" : ""
+          )}
+          onClick={() => handleTabChange("chat")}
+        >
           <p className="text-xl text-gray-200">Chat</p>
         </div>
 
-        <div className="rounded-xl flex w-1/2 justify-center">
+        {/* setting div */}
+        <div
+          className={twJoin(
+            "rounded-xl flex w-1/2 justify-center",
+            activeTab === "setting" ? "bg-blue-500" : ""
+          )}
+          onClick={() => handleTabChange("setting")}
+        >
           <p className="text-xl text-gray-200">Setting</p>
         </div>
       </div>
+
       <div className="divider mt-4 -mb-4 py-0 h-1 bg-gray-400 blur-[1px]" />
+
+      <div className="flex flex-col items-center text-gray-100 rounded-2xl p-4  w-[20vw] ">
+        {activeTab === "chat" ? <Conversations /> : <Setting />}
+      </div>
     </>
   );
 }
